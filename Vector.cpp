a6381b76038dacbfa3b7ba482778c9c748975877
Gaurav Roy <roygaurav2004@gmail.com>
2026-01-20 19:48:55 +0530

    vector implementation

---
+++ Vector.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class Vector{
+public:
+    Vector(){
+        Realloc(2);
+    }
+
+    Vector(int size, const T& value) {
+        Realloc(size);
+        m_size = size;
+        for (size_t i = 0; i < size; i++) {
+            new(&m_data[i]) T(value);
+        }
+    }
+
+    Vector(int size) : Vector(size, T()) {}
+
+    Vector(const Vector& other) {
+        Realloc(other.m_capacity);
+        for (size_t i = 0; i < other.m_size; i++) {
+            new(&m_data[i]) T(other.m_data[i]);
+        }
+        m_size = other.m_size;
+    }
+
+    Vector& operator=(const Vector& other) {
+        if (this != &other) {
+            Clear();
+            Realloc(other.m_capacity);
+            for (size_t i = 0; i < other.m_size; i++) {
+                new(&m_data[i]) T(other.m_data[i]);
+            }
+            m_size = other.m_size;
+        }
+        return *this;
+    }
+
+    Vector(Vector&& other) noexcept{
+        m_data = other.m_data;
+        m_size = other.m_size;
+        m_capacity = other.m_capacity;
+
+        other.m_data = nullptr;
+        other.m_size = 0;
+        other.m_capacity = 0;
+    }
+
+    Vector& operator=(Vector&& other) noexcept{
+        if (this != &other) {
+            Clear();
+            ::operator delete(m_data, m_capacity * sizeof(T));
+
+            m_data = other.m_data;
+            m_size = other.m_size;
+            m_capacity = other.m_capacity;
+
+            other.m_data = nullptr;
+            other.m_size = 0;
+            other.m_capacity = 0;
+        }
+        return *this;
+    }
+
+    ~Vector(){
+        Clear();
+        ::operator delete(m_data, m_capacity * sizeof(T));
+    }
+
+    void PushBack(const T& value){
+        if(m_size >= m_capacity){
+            Realloc(m_capacity + m_capacity/2);
+        }
+        m_data[m_size] = value;
+        m_size++;
+    }
+
+    template<typename... Args>
+    T& EmplaceBack(Args&&... args){
+        if(m_size >= m_capacity){
+            Realloc(m_capacity + m_capacity/2);
+        }
+        new(&m_data[m_size]) T(forward<Args>(args)...);
+        return m_data[m_size++];
+    }
+
+    void PopBack(){
+        if(m_size > 0){
+            m_size--;
+            m_data[m_size].~T();
+        }
+    }
+    
+    void Clear(){
+        for(size_t i=0; i<m_size; i++){
+            m_data[i].~T();
+        }
+        m_size = 0;
+    }
+
+
+    int Size() const {
+        return m_size;
+    }
+
+    const T& operator[](size_t index) const {
+        return m_data[index];
+    }
+
+    T& operator[](size_t index){
+        return const_cast<T&>(static_cast<const Vector&>(*this)[index]);
+    }
+
+private:
+    T* m_data = nullptr;
+    size_t m_size = 0, m_capacity = 0;
+
+    void Realloc(size_t newCapacity){
+        T* newBlock = static_cast<T*>(::operator new(newCapacity * sizeof(T)));
+        if(newCapacity < m_size) m_size = newCapacity;
+
+        for(size_t i=0; i<m_size; i++) newBlock[i] = move(m_data[i]);
+        for(size_t i=0; i<m_size; i++) m_data[i].~T();
+
+        ::operator delete(m_data, m_capacity * sizeof(T));
+        m_data = newBlock;
+        m_capacity = newCapacity;
+    }
+};
+
+template<typename T>
+void PrintVector(const Vector<T>& vec) {
+    for (size_t i = 0; i < vec.Size(); i++) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    // test all
+    Vector<int> vec;
+    vec.PushBack(1);
+    vec.PushBack(2);
+    vec.PushBack(3);
+    PrintVector(vec);
+    vec.Clear();
+    Vector<int> vec2(5, 10);
+    PrintVector(vec2);
+    // emplace
+    vec2.EmplaceBack(4);
+    vec2.EmplaceBack(5);
+    PrintVector(vec2);
+    return 0;
+}
