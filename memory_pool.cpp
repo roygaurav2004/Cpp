9aa1f194e006cecc04084bc96fb8f01390ac1181
Gaurav Roy <roygaurav2004@gmail.com>
2026-01-22 22:15:15 +0530

    memory pool

---
+++ memory_pool.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+class memory_pool {
+private:
+    union Node {
+        Node* next; // Used when the block is FREE
+        char data[sizeof(T)]; // Used when the block is ALLOCATED
+    };
+
+    Node* buf;
+    Node* front;
+
+public:
+    explicit memory_pool(size_t sz) {
+        buf = static_cast<Node*>(::operator new(sz * sizeof(Node)));
+        for (size_t i = 0; i < sz; ++i) {
+            buf[i].next = (i + 1 < sz) ? &buf[i+1] : nullptr;
+        }
+        front = buf;
+    }
+    
+    T* allocate() {
+        if (!front) return nullptr;
+        Node* node = front;
+        front = front->next; 
+        return reinterpret_cast<T*>(node->data);
+    }
+
+    void deallocate(T* ptr) {
+        Node* node = reinterpret_cast<Node*>(ptr);
+        node->next = front;
+        front = node;
+    }
+};
